9032de9d76d89a82c42c1d5d0f6df433eb77f161
HTKW-SAM <rtwking.1@gmail.com>
2024-10-11 18:26:32 +0530

    Update account.py

--- account.py
+++ account.py
@@ -1,15 +1,28 @@
 import streamlit as st
 import firebase_admin 
-from firebase_admin import credentials
-from firebase_admin import auth
+from firebase_admin import credentials, auth
 import json
-from dotenv import load_dotenv
 
-firebase_credentials = st.secrets["firebase"]["credentials"]
-cred = credentials.Certificate(json.loads(firebase_credentials))
+# Access Firebase secrets
+firebase_creds = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["firebase"]["universe_domain"]
+}
 
+# Initialize Firebase Admin
 if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_creds)
     firebase_admin.initialize_app(cred)
+
 
 
 def app():
